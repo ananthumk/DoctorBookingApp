d546240f4cb1cc8fdc27d97adee427b92662dc2b
Ananthu <ananthusmk@gmail.com>
2025-08-03 16:19:46 +0530

    done

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -2,13 +2,14 @@ import React, { useContext } from 'react'
 import { FaSearch, FaUserCircle  } from "react-icons/fa";
 import './Navbar.css'
 import { Appcontext } from '../../Context/AppContext';
+import logo from '../../assets/DBLogo.png'
 
 const Navbar = () => {
     const { searchQuery, setSearchQuery} = useContext(Appcontext)
     
     return (
         <div className='navbar-container'>
-            <h1 className='navbar-heading'>MedCare</h1>
+            <img src={logo} alt="logo" className='logo' />
             <div className='navbar-search-container'>
                 <FaSearch size={20} className='search-icon' />
                 <input type="search" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} className='navbar-search-input' placeholder='Search doctors by name and specialization' />
@@ -18,4 +19,4 @@ const Navbar = () => {
     )
 }
 
-export default Navbar
+export default Navbar 
